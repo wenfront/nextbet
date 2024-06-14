bb78008448a473099a4474c01375fdcd79db6304
webfront <webfront65@gmail.com>
2024-06-14 10:35:06 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function OpenNav(){
+    document.getElementById("nav").style.width = "60%";
+}
+    function CloseNav(){
+        document.getElementById("nav").style.width = "0%";
+}
+// slide
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
